2f5c9becc582771b0ebf0331b435a5e175f57926
SHOTA OZAKI <1029289895@example.com>
2018-10-05 16:21:16 +0900

    under Ex6

--- code/closure.ml
+++ code/closure.ml
@@ -107,6 +107,67 @@ let string_of_closure e =
   in layout (pretty 40 (pr_of_exp 0 e))
 
 
+(* convert support function *)
+let cvalue_of_nvalue = function
+    N.Var id -> Var id
+  | N.IntV i -> IntV i
+
+let ccexp_of_ncexp = function
+    N.ValExp value -> ValExp (cvalue_of_nvalue value)
+  | N.BinOp (binOp, v1, v2) -> BinOp (binOp, cvalue_of_nvalue v1, cvalue_of_nvalue v2)
+  | N.AppExp (v1, v2) -> AppExp (cvalue_of_nvalue v1, [cvalue_of_nvalue v2])
+  | N.IfExp (v, e1, e2) -> err "For debug: This error must not be raised"
+  | N.TupleExp (v1, v2) -> TupleExp [cvalue_of_nvalue v1; cvalue_of_nvalue v2]
+  | N.ProjExp (v, i) -> ProjExp (cvalue_of_nvalue v, i)
+
+(*let rec freevar (t: N.exp) id_list : MySet.t =
+  match t with*)
+
+let gather_id_from_value value (l: id list list) =
+  match value with
+    N.Var id -> 
+  | _ -> []
+
+let rec gather_id_from_cexp = function
+    N.ValExp v -> gather_id_from_value v
+  | N.BinOp (_, v1, v2)
+  | N.AppExp (v1, v2)
+  | N.TupleExp (v1, v2) -> (gather_id_from_value v1) @ (gather_id_from_value v2)
+  | N.IfExp (v, e1, e2) -> (gather_id_from_value v) @ (gather_id_from_exp e1) @ (gather_id_from_exp e2)
+  | N.ProjExp (v, _) -> gather_id_from_value v
+
+and gather_id_from_exp = function
+    N.CompExp ce -> gather_id_from_cexp ce
+  | N.LetExp (id, ce, e) 
+  | N.LoopExp (id, ce, e) -> id :: (gather_id_from_cexp ce) @ (gather_id_from_exp e)
+  | N.LetRecExp (id1, id2, e1, e2) -> [id1; id2] @ (gather_id_from_exp e1) @ (gather_id_from_exp e2)
+  | N.RecurExp v -> gather_id_from_value v
+  
 
 (* entry point *)
-let convert exp = CompExp (ValExp (IntV 1))
+let convert exp =  CompExp (ValExp (IntV 1)) 
+ (* let rec not_func_loop exp =
+    match exp with
+      N.CompExp ce ->
+       (match ce with
+          N.IfExp (v, e1, e2) -> CompExp (IfExp (cvalue_of_nvalue v, not_func_loop e1, not_func_loop e2))
+        | _ -> CompExp (ccexp_of_ncexp ce))
+    | N.LetExp (id, ce, e)
+    | N.LoopExp (id, ce, e) ->
+        let ce' =
+         (match ce with
+            N.IfExp (v, e1, e2) -> IfExp (cvalue_of_nvalue v, not_func_loop e1, not_func_loop e2)
+          | _ -> ccexp_of_ncexp ce) in
+        LetExp (id, ce', not_func_loop e)
+    | N.LetRecExp _ -> in_func_loop exp (...)
+    | N.RecurExp v -> RecurExp (cvalue_of_nvalue v)
+  and in_func_loop exp f =
+    match exp with
+      N.CompExp ce -> 
+    | N.LetExp (id, ce, e) -> 
+    | N.LetRecExp (id1, id2, e1, e2) ->
+    | N.LoopExp (id, ce, e) ->
+    | N.RecurExp v ->
+  in
+    not_func_loop exp (fun x -> x)*)
+  
