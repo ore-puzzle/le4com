96d2ff4ea5d642f5915731a7d69cba7d8cd3abda
SHOTA OZAKI <1029289895@example.com>
2018-11-16 11:40:35 +0900

    send

--- code/make_command.ml
+++ code/make_command.ml
@@ -3,13 +3,13 @@ let filenames = ref [||]
 let rec make_command = function
     [] -> ""
   | head :: rest ->
-      "gcc -o " ^ head ^ " lib/main.c " ^ head ^ ".s\n" ^ 
-      "gcc -o " ^ head ^ "opt" ^ " lib/main.c " ^ head ^ "opt.s\n" ^
+      "./" ^ head ^ "\n" ^
+      "./" ^ head ^ "opt\n" ^
       make_command rest
 let _ = 
   filenames := Sys.argv;
   let str = make_command (List.tl (Array.to_list !filenames)) in
-  let oc = open_out "assemble.sh" in
+  let oc = open_out "do.sh" in
   output_string oc str;
   close_out oc;
   
